8c6a2185737f884ccd43dad45a0ead44fbf9b08d
hungikim <gns213@gmail.com>
2023-04-14 16:08:38 -0400

    working navbar, profile section

---
+++ src/Profile.jsx
@@ -0,0 +1,9 @@
+import './Profile.css'
+
+export default function Profile() {
+    return (
+      <div class='profile'>
+        <img class='profile-photo' src='src/assets/shibe.jpg' width='150px' height='150px' />
+      </div>
+    )
+}
